eee9be0183368e4d502535f95657e7d372dda1c7
tawhid <tawhidsdreem60@gmail.com>
2025-12-27 09:11:07 +0600

    update cors api end point

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-frontend-url.vercel.app'] 
+    ? ['https://eventhub-30yk.onrender.com'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
